bb33487a3a176e74445ebef0167212d381bc0d96
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-30 13:12:06 -0400

    deploying, fixed homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
 import './App.css'
-import AboutMe from './AboutMe/AboutMe'
-import Projects from './Projects/Projects'
-import Contact from './Contact/Contact'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import MyForm from './Contact/ContactForm/MyForm'
@@ -13,7 +10,7 @@ function App() {
 		<div className='App'>
 			<Header />
 			<Switch>
-				<Route exact path='/portfolio' component={Home} />
+				<Route path='/' component={Home} />
 				<Route path='/contact' component={MyForm} />
 			</Switch>
 		</div>
