a9ee3d482e8db1a26838dbd3cd4f0ba2c1262ebf
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-30 13:11:51 -0400

    Updates

---
+++ precache-manifest.9bc39d187df259ac38d684d769cd44df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c66a91f3146b1edf605d98270a3cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14c304bb692b5585917",
+    "url": "/static/css/main.c25b17c7.chunk.css"
+  },
+  {
+    "revision": "e39234c5adc13a14f829",
+    "url": "/static/js/2.002eaed7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.002eaed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14c304bb692b5585917",
+    "url": "/static/js/main.4006724d.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "304e738f444b1f2dd38c018146d78987",
+    "url": "/static/media/bookmark-app_400x400.304e738f.png"
+  },
+  {
+    "revision": "4ebdf2d540dbdf688280a8db35f464c9",
+    "url": "/static/media/iro-branding_400x400.4ebdf2d5.png"
+  },
+  {
+    "revision": "8b3026d4c692bbdb2727ced071859c51",
+    "url": "/static/media/mobile-homebl_400x400.8b3026d4.png"
+  },
+  {
+    "revision": "db490f2cccf52c3f6f851dd5145e1195",
+    "url": "/static/media/my-portrait.db490f2c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.167b52535a8340e1304076867b797f0c.js"
+  "/precache-manifest.9bc39d187df259ac38d684d769cd44df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
