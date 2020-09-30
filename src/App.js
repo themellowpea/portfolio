0f7f2e006531f44a5d50fc5322d8cdbc897c2665
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-30 13:30:02 -0400

    deploying

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
 			<Header />
 			<Switch>
 				<Route exact path='/' component={Home} />
-				<Route path='/portolio' component={Home} />
 				<Route path='/contact' component={MyForm} />
 			</Switch>
 		</div>
