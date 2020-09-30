0979ae0bc4fdc40649f6217996c5eb2721f7fe08
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-30 13:06:19 -0400

    Updates

---
+++ precache-manifest.54a46860b8d1ce58d4a12906cdb56b95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758283d30a911ffce5d38ebe46cb175f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f3b9180210547495c3c",
+    "url": "/portfolio/static/css/main.f66083ae.chunk.css"
+  },
+  {
+    "revision": "077cb80a0631f602807e",
+    "url": "/portfolio/static/js/2.dacb98eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.dacb98eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3b9180210547495c3c",
+    "url": "/portfolio/static/js/main.e1a72697.chunk.js"
+  },
+  {
+    "revision": "8c78779c98e297eec2a1",
+    "url": "/portfolio/static/js/runtime-main.23e48518.js"
+  },
+  {
+    "revision": "304e738f444b1f2dd38c018146d78987",
+    "url": "/portfolio/static/media/bookmark-app_400x400.304e738f.png"
+  },
+  {
+    "revision": "4ebdf2d540dbdf688280a8db35f464c9",
+    "url": "/portfolio/static/media/iro-branding_400x400.4ebdf2d5.png"
+  },
+  {
+    "revision": "8b3026d4c692bbdb2727ced071859c51",
+    "url": "/portfolio/static/media/mobile-homebl_400x400.8b3026d4.png"
+  },
+  {
+    "revision": "db490f2cccf52c3f6f851dd5145e1195",
+    "url": "/portfolio/static/media/my-portrait.db490f2c.jpg"
+  }
+]);
